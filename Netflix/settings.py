4d3af34f4363970aba921c81333f9be9c8af6ea7
Muxriddin Umarqulov <79047091+Muxriddin0623@users.noreply.github.com>
2023-01-29 12:32:50 +0500

    'settings'

--- Netflix/settings.py
+++ Netflix/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'Netflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': os.environ.get('POSTGRES_DB', 'postgres',),
-#        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-#        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '3214151'),
-#        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
-#        'PORT': '5432',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', 'netflixapi',),
+        'USER': os.environ.get('POSTGRES_USER', 'developer'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '3214151'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': '5432',
+    }
+}
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.postgresql',
@@ -100,12 +100,12 @@ WSGI_APPLICATION = 'Netflix.wsgi.application'
 #    }
 #}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
