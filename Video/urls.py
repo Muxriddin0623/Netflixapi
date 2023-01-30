2fa46de12a812204163ce52031cbe4ac88f1e33d
Muxriddin Umarqulov <79047091+Muxriddin0623@users.noreply.github.com>
2023-01-30 21:19:24 +0500

    github cd

--- Video/urls.py
+++ Video/urls.py
@@ -7,14 +7,14 @@ from .views import *
 router = DefaultRouter()
 # todo_router = DefaultRouter()
 router.register('movie', MovieViewSet)
-#router.register('actors', ActorViewSet)
-#router.register('movies', MovieActorAPIView, "movies")
+router.register('actors', ActorViewSet)
+router.register('movies', MovieActorAPIView, "movies")
 router.register('comment', CommentApiView, "comment")
 
 urlpatterns = [
     #path('movies/<int:id>/actors', MovieActorAPIView.as_view(), name='movies'),
     #path('actors/', ActorAPIView.as_view(), name='actor'),
-    path("comment/", include(router.urls)),
+    #path("comment/", include(router.urls)),
     path('auth/', obtain_auth_token),
     path('', include(router.urls))
 ]
